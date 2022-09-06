e6f42b38ab627cd66de618ab905b4da9737ddf14
Roman Mátyus <matyus@efabrica.sk>
2022-09-06 19:33:17 +0200

    Test reflection

---
+++ tests/Rule/General/RequireParentConstructCallRuleTest.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types = 1);
+
+namespace Efabrica\PHPStanRules\Tests\Rule\General;
+
+use Efabrica\PHPStanRules\Rule\General\RequireParentConstructCallRule;
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use const PHP_VERSION_ID;
+
+class RequireParentConstructCallRuleTest extends RuleTestCase
+{
+
+    protected function getRule(): Rule
+    {
+        return new RequireParentConstructCallRule();
+    }
+
+    public function testCallToParentConstructor(): void
+    {
+        $this->analyse([__DIR__ . '/data/call-to-parent-constructor.php'], [
+            [
+                'IpsumCallToParentConstructor::__construct() calls parent constructor but parent does not have one.',
+                31,
+            ],
+            [
+                'BCallToParentConstructor::__construct() does not call parent constructor from ACallToParentConstructor.',
+                51,
+            ],
+            [
+                'FCallToParentConstructor::__construct() does not call parent constructor from DCallToParentConstructor.',
+                76,
+            ],
+            [
+                'BarSoapClient::__construct() does not call parent constructor from SoapClient.',
+                119,
+            ],
+            [
+                'StaticCallOnAVariable::__construct() does not call parent constructor from FooCallToParentConstructor.',
+                130,
+            ],
+        ]);
+    }
+
+    public function testCheckInTraits(): void
+    {
+        $this->analyse([__DIR__ . '/data/call-to-parent-constructor-in-trait.php'], []);
+    }
+
+    public function testCallsParentButHasNotParent(): void
+    {
+        if (PHP_VERSION_ID >= 70400) {
+            self::markTestSkipped('This test does not support PHP 7.4 or higher.');
+        }
+        $this->analyse([__DIR__ . '/data/call-to-parent-constructor-php-lt-74.php'], [
+            [
+                'CCallToParentConstructor::__construct() calls parent constructor but does not extend any class.',
+                6,
+            ],
+        ]);
+    }
+
+}
---
+++ tests/Rule/General/data/call-to-parent-constructor-php-lt-74.php
@@ -0,0 +1,11 @@
+<?php // lint <= 7.4
+
+class CCallToParentConstructor
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
